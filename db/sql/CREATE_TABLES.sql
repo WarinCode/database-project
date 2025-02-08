399c59e22aaaa108c19b2f7d8177d82031264c7d
WarinCode <warinsaipanya471@outlook.co.th>
2025-02-09 05:35:59 +0700

    Added sql files

--- db/sql/CREATE_TABLES.sql
+++ db/sql/CREATE_TABLES.sql
@@ -1,5 +1,29 @@
+USE DABASE_PROJECT;
+
 CREATE TABLE Students (
-    studentId BIGINT PRIMARY KEY,
-    studentName VARCHAR(100),
-    email VARCHAR(50)
+    Student_ID BIGINT NOT NULL PRIMARY KEY,
+    Student_Name VARCHAR(100) NOT NULL,
+    Email VARCHAR(50) NOT NULL,
 )
+
+CREATE TABLE Transactions(
+    Income FLOAT NULL,
+    Expenses FLOAT NULL,
+    Student_ID BIGINT NOT NULL,
+
+	CONSTRAINT fk_transaction
+        FOREIGN KEY(Student_ID)
+        REFERENCES Students(Student_ID),
+)
+
+CREATE TABLE DateTime_Records (
+    DT DATETIME NOT NULL,
+    Date DATE NOT NULL,
+    Time TIME NOT NULL,
+    Student_ID BIGINT NOT NULL,
+
+	CONSTRAINT fk_datetime
+        FOREIGN KEY(Student_ID)
+        REFERENCES Students(Student_ID)
+)
+
---
+++ db/sql/INSERT_STUDENTS.sql
@@ -0,0 +1,14 @@
+USE DATABASE_PROJECT;
+
+/* INSERT QUERY NO: 1 */
+INSERT INTO Students VALUES(6630250281, 'พันธุ์ธัช สุวรรณวัฒนะ', 'pantat.s@ku.th');
+
+/* INSERT QUERY NO: 2 */
+INSERT INTO Students VALUES(6630250435, 'วรินทร์ สายปัญญา', 'warin.sai@ku.th');
+
+/* INSERT QUERY NO: 3 */
+INSERT INTO Students VALUES(6630250591, 'ปุณณภพ มีฤทธิ์', 'punnaphop.m@ku.th');
+
+/* INSERT QUERY NO: 4 */
+INSERT INTO Students VALUES(6630250231, 'ปัณณวัฒน์ นิ่งเจริญ', 'pannawat.n@ku.th');
+

a32ba6b68d9d9e5f43e2b8e21e57fe9f04d64e7e
WarinCode <warinsaipanya471@outlook.co.th>
2025-03-10 21:07:41 +0700

    Update CSV data and SQL insert statements in project

--- db/sql/INSERT_STUDENTS.sql
+++ db/sql/INSERT_STUDENTS.sql
@@ -1,10 +1,10 @@
 USE DB_PROJECT;
 
 /* INSERT QUERY NO: 1 */
-INSERT INTO Students VALUES(6630250281, 'พันธุ์ธัช สุวรรณวัฒนะ', 'pantat.s@ku.th', 'S06');
+INSERT INTO Students VALUES(6630250435, 'วรินทร์ สายปัญญา', 'warin.sai@ku.th', 'S06');
 
 /* INSERT QUERY NO: 2 */
-INSERT INTO Students VALUES(6630250435, 'วรินทร์ สายปัญญา', 'warin.sai@ku.th', 'S06');
+INSERT INTO Students VALUES(6630250281, 'พันธุ์ธัช สุวรรณวัฒนะ', 'pantat.s@ku.th', 'S06');
 
 /* INSERT QUERY NO: 3 */
 INSERT INTO Students VALUES(6630250591, 'ปุณณภพ มีฤทธิ์', 'punnaphop.m@ku.th', 'S06');
